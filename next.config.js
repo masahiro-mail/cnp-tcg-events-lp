c8d20f4e29387fb70792e48a8f77a72e7d21050e
masahiro-mail <masahiro54.mail@gmail.com>
2025-08-25 20:11:49 +0900

    Add HSTS header to enforce HTTPS

--- next.config.js
+++ next.config.js
@@ -19,6 +19,35 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          },
+        ],
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+        ],
+        destination: 'https://event.cnp-tradingcard.com/$1',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
